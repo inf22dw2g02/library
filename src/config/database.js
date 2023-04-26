7a53959786299de334e6435cadcdfdb33867fe4f
Evinilson Fernandes <evinilsonsilva16@gmail.com>
2023-04-26 20:25:28 +0100

    Primeiros passos
    Está é a primeira versão do trabalho, foi criada as pastas e os ficheiros inicias para o desenvolvimento do projeto

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: '12345678',
+    database: 'dblbary',
+    define: {
+        timestamp: true,
+        underscored: true,
+        
+    }
+}
