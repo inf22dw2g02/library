1642958e8e107257d027b2a61e3dbf0a38b7e0a0
Evinilson Fernandes <evinilsonsilva16@gmail.com>
2023-04-26 20:41:17 +0100

    Inicialização do trabalho
    Criação das pastas models, config, e router e o arquivo gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# library
-This is a group assignment to show our aptitude using authorization and authentication in APIs
--- src/database/index.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const configDB = require('../config/database')
-const UserModel = require('../models/User')
-const connection = new Sequelize(configDB)
-
-UserModel.init(connection)
-module.exports = connection
