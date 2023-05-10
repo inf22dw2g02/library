f856f0f7b1a0de06be03e1898157ced735531923
Brunobk2c <bk2c@hotmail.com>
2023-05-10 17:42:16 +0100

    Implement docker and make some updates

---
+++ Project_v2/init.sql
@@ -0,0 +1,17 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS project_v2DB;
+
+-- Switch to the database
+-- USE project_v2DB;
+
+-- -- Create a table
+-- CREATE TABLE IF NOT EXISTS users (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL
+-- );
+
+-- -- Insert initial data
+-- INSERT INTO users (name, email) VALUES
+--   ('John Doe', 'john@example.com'),
+--   ('Jane Smith', 'jane@example.com');
