e4822996a89dffb4b692b9b3822d01e929e3e11f
Brunobk2c <bk2c@hotmail.com>
2023-05-11 17:50:56 +0100

    making database work into docker without issues

--- Project_v2/db/config/config.js
+++ Project_v2/db/config/config.js
@@ -1,28 +1,27 @@
 // Incluir arquivo com as variaveis de ambiente
 require('dotenv').config();
 
-//Exportar as credencias do banco de dados
+// Exportar as credencias do banco de dados
 module.exports = {
-
-  "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.MYSQL_DB,
-    "host": process.env.MYSQL_HOST,
-    "dialect": process.env.DB_DIALECT
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    host: 'db',
+    dialect: process.env.DB_DIALECT
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   },
-  "production": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.MYSQL_DB,
-    "host": process.env.MYSQL_HOST,
-    "dialect": process.env.DB_DIALECT
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DB_DIALECT
   }
-}
+};
