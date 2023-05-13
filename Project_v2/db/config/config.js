7aed4e570b69fc57fd433029ba046c1888657bd5
Brunobk2c <bk2c@hotmail.com>
2023-05-13 19:53:07 +0100

    put a time to sleep for db run first than api CMD

--- Project_v2/db/config/config.js
+++ Project_v2/db/config/config.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 // Exportar as credencias do banco de dados
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   test: {
     username: process.env.DB_USER,
